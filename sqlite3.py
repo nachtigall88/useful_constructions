f319436b53a358c1db5555614ee9c0b8b0887ed8
Roland <139322398+nachtigall88@users.noreply.github.com>
2023-08-24 12:13:15 +0300

    Update sqlite3.py

--- sqlite3.py
+++ sqlite3.py
@@ -1,1 +1,9 @@
+import sqlite as sq
 
+with sq.connect('word_data.db') as con:
+      cur = con.cursor()
+      cur.execute("""CREATE TABLE IF NOT EXIST words (
+      art TEXT,
+      word TEXT,
+      translation TEXT
+)""")
