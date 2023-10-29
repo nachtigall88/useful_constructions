3c62965234056eb70d492807bda42431c2485671
nachtigall88 <ud1narty0m123@gmail.com>
2023-10-29 19:57:56 +0200

    ddd

--- master.py
+++ master.py
@@ -1,17 +1,3 @@
-print('ok')
-
-print('off')
-
-
-def add_sth(a, b):
-    return
-
-def min_sth(a, b):
-    return a - b
-
-def print_hell0():
-    print('hello')
-
-c = 8
-q = 9
-print(c+q)
+print(1, 2, 3, '4')
+a = 2
+print(a)
